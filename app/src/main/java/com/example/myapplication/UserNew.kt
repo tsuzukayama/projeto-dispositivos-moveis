bacad4b46036de96823fb2dafb3a0feb7f4ea3f8
Matheus Teles <teles.matheusm@gmail.com>
2019-07-10 03:40:11 -0300

    Create new user screen

--- app/src/main/java/com/example/myapplication/UserNew.kt
+++ app/src/main/java/com/example/myapplication/UserNew.kt
@@ -1,13 +1,27 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class UserNew : AppCompatActivity() {
+
+    private lateinit var createUserButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.getSupportActionBar()?.hide()
         setContentView(R.layout.user_new)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        createUserButton = findViewById(R.id.create_user_button)
+
+        createUserButton.setOnClickListener{
+            Toast.makeText(App.context, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
