ce046ee6d57ed326c3bc51f2d36ced6c094f7faf
Tiago Suzukayama <tiago.suzukayama@gmail.com>
2019-07-23 20:39:01 -0300

    commit fix

--- app/src/main/java/com/example/myapplication/rides/RideList.kt
+++ app/src/main/java/com/example/myapplication/rides/RideList.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.rides
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.ActionMode
@@ -9,6 +10,7 @@ import android.widget.AbsListView
 import android.widget.BaseAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.App
 import com.example.myapplication.R
 
 class RideList :AppCompatActivity() {
@@ -26,6 +28,11 @@ class RideList :AppCompatActivity() {
         listView.adapter = RideAdapter()
 
         listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE_MODAL
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            val intent = Intent(App.context, RideDetailsPassenger::class.java)
+            startActivity(intent)
+        }
         listView.setMultiChoiceModeListener(object: AbsListView.MultiChoiceModeListener {
             override fun onItemCheckedStateChanged(mode: ActionMode, pos: Int, id: Long, checked: Boolean) {
                 mode.title = listView.checkedItemCount.toString() + " rides"
